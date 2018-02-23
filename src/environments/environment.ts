a3200f754f1ecbb21b876d3dfc9b9ea6e52ae388
lchen <lchen@addus.com>
2018-02-23 17:52:49 -0600

    the completed angular courses:completion of section 21

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyC40QggYfhqdimcUyp0S-eJaxZcoF4RTD0",
+        authDomain: "oshop-ede7d.firebaseapp.com",
+        databaseURL: "https://oshop-ede7d.firebaseio.com",
+        projectId: "oshop-ede7d",
+        storageBucket: "",
+        messagingSenderId: "223745306973"
+    }
+  };
